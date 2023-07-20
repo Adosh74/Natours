f06fd20debc7686f8872391dfbac693027e5046a
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-07-20 18:47:33 +0300

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -11,13 +11,21 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
-// 3) ROUTES
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`,
+    });
+});
 
 module.exports = app;
