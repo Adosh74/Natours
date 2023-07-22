3936bd7e28e95bb2ecf4c317d2ad607e36114df8
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-07-23 02:45:30 +0300

    Create error middlware class

---
+++ utils/appError.js
@@ -0,0 +1,13 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true; // this property is used to determine if the error is operational error or programming error.
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = AppError;
