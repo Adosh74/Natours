22359ce9a24262a378953a53a632c6fdec18cedc
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-07-26 17:35:14 +0300

    Handling database IDs

--- utils/appError.js
+++ utils/appError.js
@@ -4,7 +4,7 @@ class AppError extends Error {
 
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-        this.isOperational = true; // this property is used to determine if the error is operational error or programming error.
+        this.isOperational = true;
 
         Error.captureStackTrace(this, this.constructor);
     }
