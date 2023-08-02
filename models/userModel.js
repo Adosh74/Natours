28e903a6a022730ec81d0cda2a809539e26e1356
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-02 18:21:34 +0300

    create compare password function

--- models/userModel.js
+++ models/userModel.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         minlength: 8,
         required: [true, 'Please provide your a password'],
+        select: false,
     },
     passwordConfirm: {
         type: String,
@@ -46,6 +47,13 @@ userSchema.pre('save', async function (next) {
     this.passwordConfirm = undefined;
     next();
 });
+
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword,
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
