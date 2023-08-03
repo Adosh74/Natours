893ed6d9329cc39cb29a95576b486670541eb184
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-03 16:16:52 +0300

    create protect function for check token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -51,4 +52,44 @@ exports.login = catchAsync(async (req, res, next) => {
         status: 'success',
         token,
     });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // +[1] get the token and check if it's there
+    let token;
+    if (
+        !req.headers.authorization ||
+        !req.headers.authorization.startsWith('Bearer')
+    ) {
+        return next(
+            new AppError(
+                'You are not logged in! Please login to get access',
+                401,
+            ),
+        );
+    }
+    token = req.headers.authorization.split(' ')[1];
+
+    // +[2] Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // +[3] Check if user still exist
+    const freshUser = await User.findById(decoded.id);
+    if (!freshUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist',
+                401,
+            ),
+        );
+    }
+    // +[4] Check if user changed password after the token was issued
+    if (freshUser.passwordChangedAfter) {
+        return next(
+            AppError('User recently changed password! Please login again', 401),
+        );
+    }
+
+    req.user = freshUser;
+    next();
 });
