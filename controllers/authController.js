c6ac37a118303b27ddd523fc10a45984ce82d38e
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-07 21:05:41 +0300

    set jwt token in cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,20 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+
+    const cookieOption = {
+        expires: new Date(
+            Date.now() +
+                process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+        ),
+        httpOnly: true,
+    };
+
+    if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+
+    res.cookie('JWT', token, cookieOption);
+
+    user.password = undefined;
     res.status(statusCode).json({
         status: 'success',
         token,
@@ -29,6 +43,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         photo: req.body.photo,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
+        role: req.body.role,
     });
 
     createSendToken(newUser, 201, res);
