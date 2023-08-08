f0c7126f185373b2c21563804c60c5d851ea1d3c
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-08 21:14:55 +0300

    adding helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +11,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Global MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -22,7 +26,12 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// parse body, into req.body and set max size to 10kb
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
