993531f813260d3764abaf851ea5360807c7c13e
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-09 06:44:30 +0300

    prevent from sanitize data and xss attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,6 +30,12 @@ app.use('/api', limiter);
 
 // parse body, into req.body and set max size to 10kb
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
