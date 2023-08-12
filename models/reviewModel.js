2d7edbfa151eeb390ed037245ef6544a776e645e
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-12 22:23:09 +0300

    update review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,28 +1,40 @@
 // review, rating, createdAt, ref to tour, ref to user
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-    review: {
-        type: String,
-        required: [true, 'You must send a review'],
-        trim: true,
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review can not be empty'],
+            trim: true,
+        },
+        rating: {
+            type: Number,
+            default: 4.5,
+            min: [1, 'The rating must be above 1.0'],
+            max: [5, 'The rating must be below 5.0'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour'],
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must belong to a user'],
+        },
     },
-    rating: {
-        type: Number,
-        default: 4.5,
-        min: [1, 'The rating must be above 1.0'],
-        max: [5, 'The rating must be below 5.0'],
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
     },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    tour: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tour',
-    },
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-    },
-});
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
