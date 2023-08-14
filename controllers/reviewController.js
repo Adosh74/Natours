d4b788b29f66ed7f69be72122781ff11fe497bed
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-14 10:57:29 +0300

    creating simple nested route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -15,11 +15,14 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+    // Allow nested routes
+    if (!req.body.tour) req.body.tour = req.params.tourId;
+    if (!req.body.user) req.body.user = req.user.id;
     const newReview = await Review.create({
         review: req.body.review,
         rating: req.body.rating,
         tour: req.body.tour,
-        user: req.user.id,
+        user: req.body.user,
     });
 
     res.status(201).json({
