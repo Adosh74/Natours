daf26362818348d49cf4c6b807f03b1f88bdb7b9
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-17 04:00:30 +0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,6 +20,11 @@ exports.createUser = (req, res) => {
     });
 };
 
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
     // +[1] send error if user send a password
     if (req.body.password || req.body.passwordConfirm) {
