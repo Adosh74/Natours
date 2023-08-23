d137115a4a021ee85f6a27ca91fcf3493ed2dd20
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-23 14:24:09 +0300

    create view routes

---
+++ controllers/viewsController.js
@@ -0,0 +1,27 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+    // 1) Get tour data from collection
+    const tours = await Tour.find();
+    // 2) Build template
+    // 3) Render that template using tour data from 1)
+    res.status(200).render('overview', {
+        title: 'All Tours',
+        tours,
+    });
+});
+
+exports.getTour = catchAsync(async (req, res) => {
+    // 1) Get tour data include(Guides, Reviews)
+    const { slug } = req.params;
+    const tour = await Tour.findOne({ slug: slug }).populate({
+        path: 'reviews',
+        select: 'review rating user',
+    });
+
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour',
+        tour,
+    });
+});
