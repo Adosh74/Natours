7c0b8e7a406827c4c1bd5bd1bb21a4c114060174
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-08-23 14:23:54 +0300

    add viewRoutes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewsRoutes');
 
 const app = express();
 
@@ -66,10 +67,7 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
-app.get('/', (req, res) => {
-    res.status(200).render('base');
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
