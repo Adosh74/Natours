807c71bc066be3587848aab8ce6f5ba7ea90be9b
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-21 22:45:24 +0300

    update route

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,7 +6,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get(
-    '/checkout-session/:tourID',
+    '/checkout-session/:tourId',
     authController.protect,
     bookingController.getCheckoutSession,
 );
