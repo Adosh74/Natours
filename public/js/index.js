e232227885caa357a41056c946bcc11eebf4dff6
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-22 03:59:46 +0300

    complete all FE functionality (book)

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 
 import { login, logout } from './login';
 import { displayMap } from './mapbox';
+import { bookTour } from './stripe';
 import { updateSettings } from './updateSettings';
 
 const mapBox = document.getElementById('map');
@@ -10,6 +11,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-settings');
+const bookBtn = document.getElementById('book-tour');
 
 // values
 
@@ -64,5 +66,14 @@ if (userPasswordForm) {
         document.getElementById('password-current').value = '';
         document.getElementById('password').value = '';
         document.getElementById('password-confirm').value = '';
+    });
+}
+
+if (bookBtn) {
+    bookBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        e.target.textContent = 'Processing...';
+        const { tourId } = e.target.dataset;
+        bookTour(tourId);
     });
 }
