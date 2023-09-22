a56f0657f2dde50a4cc47dfbedba7ad5ac58fa13
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-22 03:58:05 +0300

    use stripe checkout api

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,20 +1,28 @@
 /* eslint-disable */
 import axios from 'axios';
+
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
 
-export const bookTour = async tourId => {
-  try {
-    // 1) Get checkout session from API
-    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
-    // console.log(session);
+const stripe = Stripe(
+    'pk_test_51NqQYYG5jSROj21bkxiFXThUfUAIIQwjzR3wvE8tOqyEXwB9cTA3l20tDEiKGey8H21Ah966zaieM8CKwkKg8lxe00FmFY71Ji',
+);
 
-    // 2) Create checkout form + chanre credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
-    });
-  } catch (err) {
-    console.log(err);
-    showAlert('error', err);
-  }
+export const bookTour = async (tourId) => {
+    try {
+        // 1) Get checkout session from API
+        const session = await axios({
+            method: 'GET',
+            url: `http://localhost:3001/api/v1/bookings/checkout-session/${tourId}`,
+        });
+        console.log(session);
+        // 2) Create checkout form + charge credit card
+        stripe.redirectToCheckout({
+            sessionId: session.data.session.id,
+        });
+
+        // 3) Render the form to the screen
+    } catch (error) {
+        console.log(error);
+        showAlert('error', error);
+    }
 };
