28458e292ae72f0db41aa7c53c9d8953bfca74c8
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-09-24 09:06:33 +0300

    update security options

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,20 +11,17 @@ const signToken = (id) =>
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
 
-    const cookieOption = {
+    res.cookie('JWT', token, {
         expires: new Date(
             Date.now() +
                 process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
         ),
         httpOnly: true,
-    };
-
-    if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
-
-    res.cookie('JWT', token, cookieOption);
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    });
 
     user.password = undefined;
     res.status(statusCode).json({
@@ -56,7 +53,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     const url = `${req.protocol}://${req.get('host')}/me`;
     await new Email(newUser, url).sendWelcome();
 
-    createSendToken(newUser, 201, res);
+    createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
